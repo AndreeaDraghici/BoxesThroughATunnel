73312b2aa2d047db56e4b7b8e6285870852311b3
AndreeaMariaDraghici <draghici108@gmail.com>
2021-04-07 00:37:56 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,36 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include"Box.h"
+#include"Box.c"
 #define MAX_HEIGHT 41
-
 //@author:AndreeaDraghici
 
-typedef struct box
-{
-    int length;
-    int width;
-    int height;
-} _box;
-
-int get_volume(_box v)
-{
-    return v.length*v.width*v.height;
-}
-
-//return 1 if the box's height is lower than MAX_HEIGHT
-//else return 0
-
-int max_height(_box lower)
-{
-
-    if(lower.height<MAX_HEIGHT)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
 int main()
 {
     int n;
@@ -53,7 +27,6 @@ int main()
         {
             fprintf(g,"%d\n",get_volume(boxes[i]));
         }
-
     }
     fclose(f);
     fclose(g);
