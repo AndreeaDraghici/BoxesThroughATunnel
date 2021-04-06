3abd4a3cd946f54a4027aaa16289701aeddd3976
AndreeaMariaDraghici <draghici108@gmail.com>
2021-04-07 00:37:45 +0300

    Create Box.c

---
+++ Box.c
@@ -0,0 +1,26 @@
+#include"Box.h"
+#define MAX_HEIGHT 41
+//@author:AndreeaDraghici
+
+int get_volume(_box v)
+{
+    return v.length*v.width*v.height;
+}
+
+//return 1 if the box's height is lower than MAX_HEIGHT
+//else return 0
+
+int max_height(_box lower)
+{
+
+    if(lower.height<MAX_HEIGHT)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
